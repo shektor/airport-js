42f46e0c1358ddea698c0b56634045bdc6ff5a9c
shektor <44544977+shektor@users.noreply.github.com>
2019-04-30 15:48:56 +0100

    Create Airport with empty hanger

---
+++ spec/AirportSpec.js
@@ -0,0 +1,12 @@
+describe("Airport", function() {
+  it("starts with an empty hanger", function() {
+    var airport = new Airport();
+    expect(airport._hanger.length).toEqual(0)
+  });
+  // describe("Land", function() {
+  //   it("can land a plane", function() {
+  //       var airport = new Airport();
+  //       expec
+  //   });
+  // });
+});
