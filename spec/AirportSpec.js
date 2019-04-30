a03c9baf50c2898583fb87da78260dfa76cff092
Jake Napper <napperjlg@gmail.com>
2019-04-30 16:05:08 +0100

    land plane at airport

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -3,10 +3,18 @@ describe("Airport", function() {
     var airport = new Airport();
     expect(airport._hanger.length).toEqual(0)
   });
-  // describe("Land", function() {
-  //   it("can land a plane", function() {
-  //       var airport = new Airport();
-  //       expec
-  //   });
-  // });
+  describe("Land", function() {
+
+    var plane;
+
+  beforeEach(function() {
+    plane = jasmine.createSpy('plane');
+  });
+
+    it("can land a plane", function() {
+        var airport = new Airport();
+        airport.land(plane);
+        expect(airport._hanger).toContain(plane)
+    });
+  });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,3 +1,7 @@
 var Airport = function () {
   this._hanger = [];
 };
+
+Airport.prototype.land = function (plane) {
+  this._hanger.push(plane);
+};
